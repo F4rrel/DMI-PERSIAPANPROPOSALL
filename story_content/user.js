e18d99d22cdef4e5fb274d6dcf611669c9214a9d
F4rrel <farreldwiprasetya@gmail.com>
2025-02-10 00:09:38 +0800

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,22 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6ELVviYh2sv":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSongku');
+audio.src="musikku.mp3";
+
+audio.load();
+
+audio.play();
+
+audio.volume=3.0;
+}
+
